5c4029333031645ae220c5135db47c1ff06bd0c2
Faisal Ashraf <faisalshafeeqashraf@gmail.com>
2018-04-29 17:59:30 +0100

    refactor ifs

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -4,25 +4,21 @@ namespace csharp
 {
     public class GildedRose
     {
-        IList<Item> Items;
-        public GildedRose(IList<Item> Items)
+        readonly IList<Item> Items;
+        public GildedRose(IList<Item> items)
         {
-            this.Items = Items;
+            Items = items;
         }
+
+
 
         public void UpdateQuality()
         {
             foreach (var item in Items)
             {
-                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (item.Name != "Aged Brie" && item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Name != "Sulfuras, Hand of Ragnaros"  && item.Quality > 0)
                 {
-                    if (item.Quality > 0)
-                    {
-                        if (item.Name != "Sulfuras, Hand of Ragnaros")
-                        {
                             item.Quality = item.Quality - 1;
-                        }
-                    }
                 }
                 else
                 {
@@ -56,31 +52,24 @@ namespace csharp
                     item.SellIn = item.SellIn - 1;
                 }
 
-                if (item.SellIn < 0)
+                if (item.SellIn >= 0) continue;
+
+                if (item.Name != "Aged Brie")
                 {
-                    if (item.Name != "Aged Brie")
+                    if (item.Name != "Backstage passes to a TAFKAL80ETC concert" && item.Name != "Sulfuras, Hand of Ragnaros" && item.Quality > 0)
                     {
-                        if (item.Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
-                        }
+                        item.Quality = item.Quality - 1;
                     }
                     else
                     {
-                        if (item.Quality < 50)
-                        {
-                            item.Quality = item.Quality + 1;
-                        }
+                        item.Quality = item.Quality - item.Quality;
+                    }
+                }
+                else
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
                     }
                 }
             }
